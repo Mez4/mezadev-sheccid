113b5749da5e93e0dbd7d09f4be49df42a225a43
Mez4 <mezajesus318@gmail.com>
2023-02-15 23:02:05 -0600

    hover grid Section

--- src/components/GridSection.jsx
+++ src/components/GridSection.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import florCorner from '../img/flor2.png'
 const GridSection = () => {
   return (
     <div className='main-grid-section'>
-        <div className="title-grid-section">Bellos Recuerdos</div>
-        <div className="box">Momentos</div>
-        <div className="box">Viajes</div>
-        <div className="box">Comidita</div>
+      <div className="title-grid-section">Bellos Recuerdos</div>
+      <div className="box">
+        <img className="florCorner" src={florCorner}/>
+        <p className="box-text">Momentos</p>
+      </div>
+      <div className="box">
+      <img className="florCorner" src={florCorner}/>
+        <p className="box-text">Viajes</p>
+      </div>
+      <div className="box">
+      <img className="florCorner" src={florCorner}/>
+        <p className="box-text">Comidita</p>
+      </div>
     </div>
   )
 }
